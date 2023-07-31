8b7bfa9533486ba9ed462b96812bd666e06e72e4
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-07-31 23:34:42 +0000

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,9 +1,9 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Brainstorma <https://github.com/Brainstorma>
  * @returns {Set}
- */
+ **/
 export default function setFromArray(array) {
   return new Set(array);
 }
