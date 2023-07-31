0c99860d8a0ca70dfa47d240641bb46956673495
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-07-31 23:48:57 +0000

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -8,7 +8,7 @@
  * @param {String} city - The location.
  * @author Brainstorma <https://github.com/Brainstorma>
  * @returns
- **/
+ */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location === city);
