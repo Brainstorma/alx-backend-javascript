9ab9d194e6a2f7bbc3db1294f4233b30627434fd
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-07-31 23:28:20 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Brainstorma <https://github.com/Brainstorma>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
