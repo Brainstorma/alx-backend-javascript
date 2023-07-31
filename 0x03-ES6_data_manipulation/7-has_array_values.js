249c8b2ea5c4dda8deefe52344cc2b51a1abf752
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-07-31 23:53:30 +0000

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -4,7 +4,7 @@
  * @param {*} array - The array of items.
  * @author Brainstorma <https://github.com/Brainstorma>
  * @returns {Boolean}
- **/
+ */
 export default function hasValuesFromArray(set, array) {
   return array.every((value) => set.has(value));
 }
